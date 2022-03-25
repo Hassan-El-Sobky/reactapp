6d10791527e1397ba49e319cf39f37caf5528312
Hassan-El-Sobky <hassan_elsobky@hotmail.com>
2022-03-25 17:07:30 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learne React
         </a>
       </header>
     </div>
